2d9a883d78b0df1a4653714d4ebc85edf3b114fc
Serkan Kumru <serkankumru1997@gmail.com>
2023-09-15 11:59:09 +0300

    Add files via upload

---
+++ MyValidation.cs
@@ -0,0 +1,28 @@
+﻿using DAL.Repository;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Haberler.Validation
+{
+    public class MyValidation : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            if (value != null)
+            {
+                EditorRepository edt = new EditorRepository();
+                foreach (var item in edt.List())
+                {
+                    if (value.ToString() == item.UserName)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
