3010398c69641e98d962b6456985c095eb5a16a3
Serkan Kumru <serkankumru1997@gmail.com>
2023-09-16 12:15:19 +0300

    Update UserViewModel.cs

--- UserViewModel.cs
+++ UserViewModel.cs
@@ -19,5 +19,24 @@ namespace FirstMVC.Models
         public DateTime CreateDate { get; set; }
         //[Range(5,15,ErrorMessage ="bişeyler bişeyler.")]
         public int UserType { get; set; }
+
+[Required]
+[Range(1,int.MaxValue,ErrorMessage ="Product must be selected")]
+public int ProductId { get; set; }
+
+public int BannerType { get; set; }//0:head,1bottom,2middle
+
+[ForeignKey("ProductId")]
+public virtual Product Product { get; set; }
+
+[NotMapped]
+public string ImageUrl
+{
+    get
+    {
+        return "/MyImages/" + Product.Id + "_" + Product.ProductImages.First(c => c.IsMain).Id+".jpg";
+    }
+}
+        
     }
 }
